b4f07693cd0414018a8d1d0bcc713debf0fc1231
Thijs Borst <thijs.borst@incentro.com>
2015-08-18 19:58:47 +0200

    Updated AssemblyInfo.

--- source/DD4T.DI.Autofac/Properties/AssemblyInfo.cs
+++ source/DD4T.DI.Autofac/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("DD4T.Bootstrap.Autofac")]
+[assembly: AssemblyTitle("DD4T.DI.Autofac")]
 [assembly: AssemblyProduct("DD4T")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Open Source")]
